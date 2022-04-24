617b6c01657308bfeda1b7170f9a7e28550c181c
Euclides <euclides981@gmail.com>
2022-04-24 20:32:01 -0300

    Remoção dos Alerts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
+document.querySelector('#entrada').focus();
+
 document.querySelector('#criptografar').addEventListener('click', () => {
+
+    document.querySelector('#entrada').focus();
 
     var txtDigitado = document.querySelector('#entrada');
 
@@ -13,13 +17,14 @@ document.querySelector('#criptografar').addEventListener('click', () => {
         document.querySelector('#saida').value = txtCriptografado;
         txtDigitado.value = "";
     } else {
-        alert("Digite Algo na Caixa para Criptografar");
-        /* document.querySelector('#saida').value = "Digite Algo na Caixa ao Lado para Criptografar"; */
+        document.getElementById('aviso').innerHTML = "Digite Algo na Primeira Caixa para Criptografar";
     }
 });
 
 
 document.querySelector('#descriptografar').addEventListener('click', () => {
+
+    document.querySelector('#entrada').focus();
 
     var txtDigitado = document.querySelector('#entrada');
 
@@ -33,19 +38,20 @@ document.querySelector('#descriptografar').addEventListener('click', () => {
         document.querySelector('#saida').value = txtCriptografado;
         txtDigitado.value = "";
     } else {
-        alert("Digite Algo na Caixa para Descriptografar");
-        /* document.querySelector('#saida').value = "Digite Algo na Caixa ao Lado para Descriptografar"; */
+        document.getElementById('aviso').innerHTML = "Cole um Texto Criptografado na Primeira Caixa para Descriptografar";
     }
 });
 
 /* Botão CTRL+C */
 
 document.getElementById('ctrlc').addEventListener('click', clipboardCut);
+
 async function clipboardCut() {
 let text = document.querySelector("#saida").value;
 await navigator.clipboard.writeText(text);
 document.querySelector('#saida').value = "";
 document.querySelector('#entrada').value = text;
+document.querySelector('#entrada').focus();
 }
 
 /* Botão Limpar */
@@ -53,14 +59,16 @@ document.querySelector('#entrada').value = text;
 document.querySelector('#limp').addEventListener('click', () => {
 document.querySelector('#saida').value = "";
 document.querySelector('#entrada').value = "";
+document.getElementById('aviso').innerHTML = "&nbsp;";
+document.querySelector('#entrada').focus();
 });
 
 function share(){
 	if (navigator.share !== undefined) {
 		navigator.share({
-			title: 'Encriptador',
-			text: 'Projeto de um Encriptador de Texto',
-			url: 'https://euclides981.github.io/criptografia/',
+			title: '',
+			text: '',
+			url: '',
 		})
 		.then(() => console.log('Successful share'))
 		.catch((error) => console.log('Error sharing', error));
